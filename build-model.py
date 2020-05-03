3dfe6def37f0dab772b90e21b536da2b20af0307
orshalit <orshalit93@gmail.com>
2020-05-03 15:00:03 +0300

    initial commit

--- build-model.py
+++ build-model.py
@@ -14,6 +14,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Dense, Conv2D, MaxPool2D , Flatten,MaxPooling2D
 from keras.models import load_model, Model
+from keras import optimizers
 from keras.optimizers import Adam
 from keras.utils import np_utils, to_categorical
 import matplotlib.pyplot as plt
@@ -21,6 +22,7 @@ from skimage import data, color
 from skimage.transform import rescale, resize, downscale_local_mean
 from skimage.io import imread, imshow
 import pandas as pd
+from keras.applications.resnet_v2 import ResNet50V2
 from keras.preprocessing import image
 import kerasModel
 from DataGeneratorClass import My_Custom_Generator
@@ -29,7 +31,7 @@ random.seed(0)
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'trained_model.h5'
 labels = images.getLabels()
-batch_size = 32
+batch_size = 64
 input_shape = (40, 40, 3)
 num_classes = len(labels)
 #=======================================================================================
@@ -57,8 +59,7 @@ y_test = np.load('y_test.npy')
 X_val_filenames = np.load('X_val_filenames.npy')
 y_val = np.load('y_val.npy')
 
-print(len(y_val))
-exit(1)
+
 '''send train and test to the Generator'''
 my_training_batch_generator = My_Custom_Generator(X_train_filenames, y_train, batch_size,input_shape)
 my_test_batch_generator = My_Custom_Generator(X_test_filenames, y_test, batch_size, input_shape)
@@ -67,35 +68,76 @@ my_val_batch_generator = My_Custom_Generator(X_val_filenames, y_val, batch_size,
 
 '''build model or if model exists lets predict and run evaluations'''
 if glob.glob(save_dir+'/*'):#TODO: make this work with the new architecture
-    my_model = load_model(save_dir+'/'+model_name)
-    # n_test = my_test_batch_generator.getNumber()
-    # y_pred1 = Model.predict_generator(my_model,my_test_batch_generator,n_test//batch_size)
-    # print('y_pred1 :', y_pred1)
-    # y_pred = np.argmax(y_pred1, axis=1)
+    # my_model = load_model(save_dir+'/'+model_name)
+    my_model = load_model('img_aug_cnn_restnet50.h5')
+    n_test = my_test_batch_generator.getNumber()
+    y_pred1 = Model.predict_generator(my_model,my_test_batch_generator,n_test/batch_size)
+    # score = Model.evaluate_generator(my_model, my_test_batch_generator, n_test // batch_size)
+    # print('Score: ',score)
+
+    print('y_pred1 :', y_pred1)
+    y_pred = np.argmax(y_pred1, axis=1)
+    y_test = np.argmax(y_test, axis=1)
     # print('y_pred :', y_pred)
-    # # Print f1, precision, and recall scores
+    # print('y_test :', y_test)
+    # print('y_pred1 len: ', len(y_pred1))
+    # print('y_pred len: ',len(y_pred))
+    # print('y_test len: ',len(y_test))
+    # Print f1, precision, and recall scores
     # print(metrics.precision_score(y_test, y_pred, average="micro"))
     # print(metrics.recall_score(y_test, y_pred, average="micro"))
-    # print(metrics.f1_score(y_test, y_pred, average="weighted"))
+    print(metrics.f1_score(y_test, y_pred, average="macro"))
+    print(metrics.f1_score(y_test, y_pred, average="micro"))
     #
-    # print(metrics.confusion_matrix(y_test, y_pred))
+    print(metrics.confusion_matrix(y_test, y_pred))
 
-    img = image.load_img("akoya1.png", target_size=(224, 224))
-    img = np.asarray(img)
-    plt.imshow(img)
-    img = np.expand_dims(img, axis=0)
-
-
-    output = my_model.predict(img)
-    output = np.argmax(output)
-    print(output)
-    print(integer_to_label[str(output)])
-
+    # img = image.load_img("akoya1.png", target_size=(40, 40))
+    # img = np.asarray(img)
+    # plt.imshow(img)
+    # img = np.expand_dims(img, axis=0)
+    #
+    #
+    # output = my_model.predict(img)
+    # output = np.argmax(output)
+    # print(output)
+    # print(integer_to_label[str(output)])
+    #
+    # restnet = ResNet50V2(include_top=False, weights=None, input_shape=input_shape,classes=len(integer_to_label))
+    # output = restnet.layers[-1].output
+    # output = keras.layers.Flatten()(output)
+    # restnet = Model(restnet.input, output=output)
+    # for layer in restnet.layers:
+    #     layer.trainable = False
+    # restnet.summary()
+    # model = Sequential()
+    # model.add(restnet)
+    # model.add(Dense(512, activation='relu', input_dim=input_shape))
+    # model.add(Dropout(0.3))
+    # model.add(Dense(512, activation='relu'))
+    # model.add(Dropout(0.3))
+    # model.add(Dense(4, activation='softmax'))
+    # model.compile(loss=keras.losses.categorical_crossentropy,
+    #               optimizer=optimizers.RMSprop(lr=2e-5),
+    #               metrics=['accuracy'])
+    # model.summary()
+    #
+    # history = model.fit_generator(my_training_batch_generator,
+    #                               steps_per_epoch=100,
+    #                               epochs=2,
+    #                               validation_data=my_val_batch_generator,
+    #                               validation_steps=50,
+    #                               verbose=1)
+    # model.save('img_aug_cnn_restnet50.h5')
 
 else:
-    print("Buidling model......")
-    kerasModel.pearl_type_model(my_training_batch_generator
+    print("Buidling VGG16 model......")
+    kerasModel.pearl_type_model_vgg16(my_training_batch_generator
                                 ,my_val_batch_generator
                                 ,save_dir,model_name,batch_size,input_shape)
+
+    print("Buidling ResNet50V2 model......")
+    kerasModel.pearl_type_model_vgg16(my_training_batch_generator
+                                      , my_val_batch_generator
+                                      , save_dir, model_name, batch_size, input_shape)
 
 
--- kerasModel.py
+++ kerasModel.py
@@ -12,11 +12,11 @@ from keras.layers import Input, Conv1D, Conv2D, MaxPooling1D, MaxPool2D, Dense, 
 from keras.optimizers import Adam
 from keras.layers.normalization import BatchNormalization
 from keras.utils import to_categorical
-
+from keras.applications.resnet_v2 import ResNet50V2
 from DataGeneratorClass import My_Custom_Generator
+from keras import optimizers
 
-
-def pearl_type_model(my_training_batch_generator, my_test_batch_generator, save_dir=os.path.join(os.getcwd(), 'saved_models')
+def pearl_type_model_vgg16(my_training_batch_generator, my_test_batch_generator, save_dir=os.path.join(os.getcwd(), 'saved_models')
                      , model_name='trained_model.h5'
                      , batch_size=32
                      , input_shape=(40,40,3)):
@@ -45,7 +45,7 @@ def pearl_type_model(my_training_batch_generator, my_test_batch_generator, save_
     model.add(Dense(units=4096,activation="relu"))
     model.add(Dense(units=4096,activation="relu"))
     model.add(Dense(units=4, activation="softmax"))
-    opt = Adam(lr=0.001)
+    opt = Adam(lr=0.01 )
     model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
     model.summary()
@@ -53,7 +53,6 @@ def pearl_type_model(my_training_batch_generator, my_test_batch_generator, save_
 
     n_train = My_Custom_Generator.getNumber(my_training_batch_generator)
     n_test = My_Custom_Generator.getNumber(my_test_batch_generator)
-    print('n_val: ',n_test)
     print('number of training images: ',n_train)
     print('number of val images: ', n_test)
     model.fit_generator(generator=my_training_batch_generator,
@@ -62,6 +61,43 @@ def pearl_type_model(my_training_batch_generator, my_test_batch_generator, save_
                         verbose = 1,
                         validation_data = my_test_batch_generator,
                         validation_steps = int(n_test // batch_size))
+
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    model_path = os.path.join(save_dir, model_name)
+    model.save(model_path)
+    print('Saved trained model at %s ' % model_path)
+
+def pearl_type_model_resnet50v2(my_training_batch_generator, my_test_batch_generator,save_dir=os.path.join(os.getcwd(), 'saved_models')
+                         , model_name='trained_model_resnet50v2.h5'
+                         , batch_size=32
+                         , input_shape=(40, 40, 3)):
+    restnet = ResNet50V2(include_top=False, weights=None, input_shape=input_shape, classes=len(integer_to_label))
+    output = restnet.layers[-1].output
+    output = keras.layers.Flatten()(output)
+    restnet = Model(restnet.input, output=output)
+    for layer in restnet.layers:
+        layer.trainable = False
+    restnet.summary()
+    model = Sequential()
+    model.add(restnet)
+    model.add(Dense(512, activation='relu', input_dim=input_shape))
+    model.add(Dropout(0.3))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(4, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=optimizers.RMSprop(lr=2e-5),
+                  metrics=['accuracy'])
+    model.summary()
+
+    history = model.fit_generator(my_training_batch_generator,
+                                  steps_per_epoch=100,
+                                  epochs=2,
+                                  validation_data=my_test_batch_generator,
+                                  validation_steps=50,
+                                  verbose=1)
+    model.save('trained_model_resnet50v2.h5')
 
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
