4ef0cc1c9492d677fc333982a10c61cb6a65eeba
orshalit <orshalit93@gmail.com>
2020-06-19 09:47:27 +0300

    simple gui

--- guiProj.py
+++ guiProj.py
@@ -1,13 +1,13 @@
 import PySimpleGUI as sg
-
+import os
 
 sg.theme('Dark Blue 3')
-
+image_elem = sg.Image(filename='')
 layout = [[sg.Text('Choose an image to classify: ',font=("Helvetica", 30))],
           [sg.Input(), sg.FileBrowse(key='img')],
-          [sg.Button('Classify My Pearl')]
+          [sg.Button('Classify My Pearl')],
+          [image_elem]
 ]
-
 
 window = sg.Window('Pearls Classifier', layout, size=(600, 600), auto_size_text=True, auto_size_buttons=True, margins=(30,30), finalize=True)
 
@@ -18,12 +18,16 @@ while True:
         break
     try:
         ImageToClassify = str(values["img"])
+
     except:
         pass
-        #ImageToClassify = None
     if event == 'Classify My Pearl':
         if ImageToClassify == '':  #string of the path of the image
             sg.popup('Please choose an image!!!')
         else:
+            print(ImageToClassify)
+            #image_elem.Update(filename=ImageToClassify)
             print("here is the part to classify the image")
+            res = "Your pearl is ***** pearl, congrutulations!!"
+            sg.popup(res, title='Result', font=("Helvetica", 25))
 window.close()
